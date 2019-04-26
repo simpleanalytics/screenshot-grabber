ec0f1da1f5bd9084198d16017fba43dea2ca404a
Adriaan van Rossum <git@adriaan.io>
2019-04-26 21:15:46 +0800

    Remove product hunt cookie banner

--- utils/cleaners/style.js
+++ utils/cleaners/style.js
@@ -1,6 +1,6 @@
 /* eslint-env browser */
 
-module.exports = (style, value) => new Promise(resolve => {
+module.exports = (selector, style, value) => new Promise(resolve => {
   function getStyle(el, prop) {
     var view = document.defaultView
     if (view && view.getComputedStyle) {
@@ -8,18 +8,18 @@ module.exports = (style, value) => new Promise(resolve => {
     }
     return el.currentStyle[prop]
   }
-  function getElementByStyle(style, value, tag) {
-    var all = document.getElementsByTagName(tag || '*')
+  function getElementByStyle(style, value, selector) {
+    var all = document.querySelectorAll(selector || '*')
     var len = all.length
     var result = []
-    for ( var i = 0; i < len; i++ ) {
-      if ( getStyle(all[i], style) === value )
+    for (var i = 0; i < len; i++) {
+      if (getStyle(all[i], style) === value)
         result.push(all[i])
     }
     return result
   }
 
-  var elements = getElementByStyle(style, value)
+  var elements = getElementByStyle(style, value, selector || '*')
   if (elements.length === 0) return resolve(false)
   elements[0].remove()
   resolve(true)
