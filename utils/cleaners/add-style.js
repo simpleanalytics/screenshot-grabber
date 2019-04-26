ac961c50d813577aebd1fc9ad87fc885f0268a41
Adriaan van Rossum <git@adriaan.io>
2019-04-26 20:23:57 +0800

    Add max-height style to prevent infinite scrolling

--- utils/cleaners/add-style.js
+++ utils/cleaners/add-style.js
@@ -1,8 +1,8 @@
 /* eslint-env browser */
 
-module.exports = (selector, style, value) => new Promise(resolve => {
-  const element = document.querySelector(selector)
-  if (!element) resolve(false)
-  element.style[style] = value
+module.exports = (selectors, style, value) => new Promise(resolve => {
+  const elements = document.querySelectorAll(selectors)
+  if (!elements || !elements.length) resolve(false)
+  ;[...elements].forEach(element => element.style[style] = value)
   resolve(true)
 })
